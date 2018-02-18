1ba730fc194ac864b1fa64ca0a468aab1a68edbc
Mike Morrone <Hanegwen725@gmail.com>
2018-02-18 16:00:38 -0600

    Started Menu 2.0

--- Assets/Scripts/MenuSystem.cs
+++ Assets/Scripts/MenuSystem.cs
@@ -5,24 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
-    public void Credits()
-    {
-        SceneManager.LoadScene("Credits");
-    }
-
-    public void Instructions()
-    {
-        SceneManager.LoadScene("Instructions");
-    }
 
     public void Level()
     {
         SceneManager.LoadScene("Level");
-    }
-
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
     }
 
 }
